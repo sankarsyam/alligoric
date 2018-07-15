0856fc13686b0f277180fef850b64104c52e8d2a
syamcts <syam.sankar2@cognizant.com>
2018-07-15 22:11:58 +0530

    user page added

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,79 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import '../assets/scss/Users.css';
 import Card from 'react-md/lib/Cards/Card';
 import CardText from 'react-md/lib/Cards/CardText';
 import CardTitle from 'react-md/lib/Cards/CardTitle';
 import Divider from 'react-md/lib/Dividers';
+import { Button } from 'react-md';
 
-const Users = () => (
-  <div className="UsersPage">
-    <Card className="new-project page-container">
-      <CardTitle
-        className="users-page__title page-container__title"
-        title="Users"
-      />
-      <Divider />
-      <div className="users-page_list" />
-    </Card>
-  </div>
-);
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { collapse: false };
+    this.handleToggleCollapse = this.handleToggleCollapse.bind(this);
+  }
+
+  handleToggleCollapse() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  getHeader = () => {
+    const { projectType } = this.props;
+    return (
+      <tr className="user-page_table-row">
+        <td className="">
+          <Button
+            className="dashboard-header__arrow"
+            icon
+            onClick={this.handleToggleCollapse}
+          >
+            {this.state.collapse ? 'arrow_drop_down' : 'arrow_drop_up'}
+          </Button>
+        </td>
+      </tr>
+    );
+  };
+
+  getUserBasicDetails = () => {
+    const { projectType } = this.props;
+    return (
+      <tbody className={this.state.collapse ? 'collapse' : ''}>
+        <span>Hello</span>
+        <br />
+        <span>Hello</span>
+        <br />
+        <span>Hello</span>
+        <br />
+        <span>Hello</span>
+        <br />
+        <span>Hello</span>
+        <br />
+      </tbody>
+    );
+  };
+
+  render() {
+    const { projectType } = this.props;
+    return (
+      <div className="UsersPage">
+        <Card className="users-page page-container">
+          <CardTitle
+            className="users-page__title page-container__title"
+            title="Users"
+          />
+          <Divider />
+          <div className="users-page_list">
+            <table className="user-page_table">
+              {this.getHeader()}
+              {this.getUserBasicDetails()}
+            </table>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
+
+Users.propTypes = {};
 
 export default Users;
